ecabbbf2f538bc47b34df885d453c5e6c7dbc372
Sousa <daysasousa108@gmail.com>
2026-08-30 22:15:25 -0300

    testando uma possível solução para o problema usado  php

---
+++ D.3.2/index.php
@@ -0,0 +1,39 @@
+<?php 
+   echo "hello wolrd!"
+?>
+
+<?php
+	$nameCompleto = maria fernada;
+	$idade = 20;
+	echo "$nameCompleto";
+	
+	if($idade <= 18){
+	  echo "voce tem $idade";
+	  echo "entao es menor de idade";
+	}
+	else{
+	  echo "voce tem $idade";
+	  echo "entao es maior de idade";
+	}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	$nameCompleto = "Augusta Ada Byron King.";
+	$idade = 70;
+	echo "Seu nome completo é $nameCompleto ";
+   
+	
+	if($idade <= 18){
+	  echo "voce tem. $idade\n .";
+	  echo "entao você é menor de idade.\n";
+	}
+	else{
+	  echo "voce tem $idade . ";
+	  echo "entao você é maior de idade.\n";
+	}
+?>
